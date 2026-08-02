043d45da33a43740664a856486b457bdab279f95
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-08-02 22:31:03 +0530

    weather application

---
+++ some projects/weather.js
@@ -0,0 +1,4 @@
+const searchBox=document.querySelector(".search-box");
+const enterButton=document.querySelector(".enterbutton");
+const cityName=document.querySelector(".cityName");
+const apiKey="";
